d697083e35cadb374c1e71cecadd9c5210afe9bb
Saad Qureshi <saad.qureshi51.sq@gmail.com>
2023-10-09 19:00:23 +0500

    basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <ThemeProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </ThemeProvider>,
